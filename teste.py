1844200df8e8bd7ec6f84e4bf5de4d06afda1462
Peter442002 <hugofpl0@gmail.com>
2022-04-28 01:51:49 -0300

    1

--- teste.py
+++ teste.py
@@ -1,11 +1,15 @@
+import PyPDF2 as py2
 
-print("foda-Se")
-x = input()
-while x != "nao":
-    print("ja acabou jessica?")
-    x = input()
+pdf = open("pdfteste.pdf", "rb")
 
+pdf_reader = py2.pdfFileReader(pdf)
 
+n = pd_reader.numPages
+
+for i in range(0, n):
+    print("Página {}".format(i + 1))
+    page = pdf_reader.getPage(i)
+    print(page.extractText())
 
 
 
